dfa2d9168a8e053c2e4e726bcdadea5c1fa57dca
Kocsis Gergely <kocsisger@example.com>
2025-11-20 09:14:24 +0100

    Initial commit

---
+++ src/main/java/hu/unideb/inf/bootstrap_cs8/BootstrapCs8Application.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.bootstrap_cs8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapCs8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapCs8Application.class, args);
+    }
+
+}
