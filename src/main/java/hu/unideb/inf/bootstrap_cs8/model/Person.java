ae5b5f3ed1cbb982bffa721671a42675f9519ded
Kocsis Gergely <kocsisger@example.com>
2025-11-20 09:33:30 +0100

    Person

---
+++ src/main/java/hu/unideb/inf/bootstrap_cs8/model/Person.java
@@ -0,0 +1,26 @@
+package hu.unideb.inf.bootstrap_cs8.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    @Id
+    @GeneratedValue
+    Integer id;
+    String firstName;
+    String lastName;
+    String email;
+    String password;
+    boolean enabled;
+}
