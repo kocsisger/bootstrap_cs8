3e9f68bf9eedd12868a4b88db532aec8e2262ea7
Kocsis Gergely <kocsisger@example.com>
2025-11-27 08:26:35 +0100

    bootstrap added

---
+++ src/main/java/hu/unideb/inf/bootstrap_cs8/controller/MainController.java
@@ -0,0 +1,12 @@
+package hu.unideb.inf.bootstrap_cs8.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("")
+    public String showHomepage(){
+        return "index";
+    }
+}
