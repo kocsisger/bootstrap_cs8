21399f0e1774e4fd8dd84215ecbc38b8cac63671
Kocsis Gergely <kocsisger@example.com>
2025-11-27 09:38:35 +0100

    C R U D pipa

--- src/main/java/hu/unideb/inf/bootstrap_cs8/controller/MainController.java
+++ src/main/java/hu/unideb/inf/bootstrap_cs8/controller/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -33,9 +34,21 @@ public class MainController {
         return "newPersonForm";
     }
 
+    @GetMapping("/persons/update/{id}")
+    public String updatePerson(@PathVariable int id, Model model){
+        model.addAttribute("newPerson", personRepository.findById(id).get());
+        return "newPersonForm";
+    }
+
     @PostMapping("/persons/save")
     public String savePerson(Person newPerson){
         personRepository.save(newPerson);
+        return "redirect:/persons";
+    }
+
+    @GetMapping("/persons/delete/{id}")
+    public String deletePerson(@PathVariable int id){
+        personRepository.deleteById(id);
         return "redirect:/persons";
     }
 }
