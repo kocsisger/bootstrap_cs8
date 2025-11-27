c3443a7d2884ee63acc6ee22d78bacaf1b494d29
Kocsis Gergely <kocsisger@example.com>
2025-11-27 09:23:12 +0100

    create added

--- src/main/java/hu/unideb/inf/bootstrap_cs8/controller/MainController.java
+++ src/main/java/hu/unideb/inf/bootstrap_cs8/controller/MainController.java
@@ -1,17 +1,41 @@
 package hu.unideb.inf.bootstrap_cs8.controller;
 
+import hu.unideb.inf.bootstrap_cs8.model.Person;
+import hu.unideb.inf.bootstrap_cs8.model.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
+    @Autowired
+    PersonRepository personRepository;
+
     @GetMapping("")
     public String showHomepage(){
         return "index";
     }
 
     @GetMapping("/persons")
-    public String listAllPersons(){
+    public String listAllPersons(Model model){
+        List<Person> personsList = personRepository.findAll();
+        model.addAttribute("personsList", personsList);
         return "persons";
+    }
+
+    @GetMapping("/persons/new")
+    public String newPerson(Model model){
+        model.addAttribute("newPerson", new Person());
+        return "newPersonForm";
+    }
+
+    @PostMapping("/persons/save")
+    public String savePerson(Person newPerson){
+        personRepository.save(newPerson);
+        return "redirect:/persons";
     }
 }
